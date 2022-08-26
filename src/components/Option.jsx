8d2e3595cf76ba7c5c9f3612f760e75844134a4a
Julio99 <Julioespinola9999@gmail.com>
2022-08-26 08:57:58 -0400

    commit3

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,12 +1,23 @@
 import styles from "../stylesheets/Option.module.scss";
 
-export function Option(props){
-    return(
-        <div className={`${styles.option} ${styles[props.option]}`}>
-            <div className={`${styles.wrapper}`}>
-                <span className={`${styles.wrapper__item} `}></span>
+export function Option(props) {
+    if (props.option.listen) {
+        return (
+            <div className={`${styles.option} ${styles[props.option.element]}`} id={props.option.element} onClick={props.handleClick}>
+                <div className={`${styles.wrapper}`}>
+                    <span className={`${styles.wrapper__item} `}></span>
+                </div>
+                <span className={styles.picked}></span>
             </div>
-            <span className={styles.picked}></span>
-        </div>
-    );
+        );
+    } else {
+        return(
+            <div className={`${styles.option} ${styles[props.option.element]}`} id={props.option.element}>
+                <div className={`${styles.wrapper}`}>
+                    <span className={`${styles.wrapper__item} `}></span>
+                </div>
+                <span className={styles.picked}></span>
+            </div>
+        );
+    }
 }
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,14 +1,54 @@
 import styles from "../stylesheets/Options.module.scss";
 import { Option } from "../components/Option";
+import { useEffect, useState } from "react";
 
 export function Options(){
+    const items = [{element:"scissors", listen: true}, {element:"spock", listen:true}, {element:"papper", listen:true}, {element:"lizard", listen:true}, {element:"rock", listen:true}]
+    const [options, setOptions] = useState(items);
+    const [background, setBackground] = useState(true);
+    const [result, setResult] = useState(false);
+
+    function handleClick(e){
+        setOptions([{element:e.currentTarget.id, listen:false}]);
+        setBackground(false);
+
+        setTimeout(()=>{
+            const number = Math.floor(Math.random()*(4+1));
+            setOptions(current => current.concat({element: items[number].element, listen: false}));
+            setTimeout(()=>{
+                setResult(true);
+            }, 300)
+        },1000)
+    }
+
+    function restart (){
+        setOptions(items);
+        setBackground(true);
+    }
+
+    function calc (el1,el2){
+        console.log(el1, el2);
+    }
+
+    useEffect(()=>{
+        if(options.length === 2){
+            calc(options[0].element, options[1].element);
+        }
+    }, [options])
+
+
     return(
-        <section className={styles.options}>
-            <Option option="scissors" />
-            <Option option="spock" />
-            <Option option="papper" />
-            <Option option="lizard" />
-            <Option option="rock" />
-        </section>
+        <>
+            <section className={background ? styles.options: `${styles.options} ${styles.noBackground}`}>
+                {
+                    options.map((option,index) => <Option key={index} option={option} handleClick={handleClick} />)
+                }
+
+            </section>
+            <div className={result ? styles.result: ''}>
+                You Lose
+                <button onClick={restart}>Play Again</button>
+            </div>
+        </>
     );
 }
