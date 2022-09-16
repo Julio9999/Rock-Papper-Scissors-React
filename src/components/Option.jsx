08431d746efaaf9dede1b3f066c395d74d33b1fb
Julio99 <Julioespinola9999@gmail.com>
2022-09-16 12:02:12 -0400

    commit4

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,18 +1,19 @@
 import styles from "../stylesheets/Option.module.scss";
 
 export function Option(props) {
+
     if (props.option.listen) {
         return (
-            <div className={`${styles.option} ${styles[props.option.element]}`} id={props.option.element} onClick={props.handleClick}>
+            <div className={`${styles.option} ${styles[props.option.element]}`} id={props.option.element} onClick={props.handleClick} >
                 <div className={`${styles.wrapper}`}>
-                    <span className={`${styles.wrapper__item} `}></span>
+                    <span className={`${styles.wrapper__item}`}></span>
                 </div>
                 <span className={styles.picked}></span>
             </div>
         );
     } else {
         return(
-            <div className={`${styles.option} ${styles[props.option.element]}`} id={props.option.element}>
+            <div className={`${styles.option} ${styles[props.option.element]}`} id={props.option.element} beats={props.option.beats}>
                 <div className={`${styles.wrapper}`}>
                     <span className={`${styles.wrapper__item} `}></span>
                 </div>
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -3,52 +3,102 @@ import { Option } from "../components/Option";
 import { useEffect, useState } from "react";
 
 export function Options(){
-    const items = [{element:"scissors", listen: true}, {element:"spock", listen:true}, {element:"papper", listen:true}, {element:"lizard", listen:true}, {element:"rock", listen:true}]
+    const items = [{element:"scissors", listen: true, beats : ['papper', 'lizard'], win:''},
+    {element:"spock", listen:true, beats : ['scissors', 'rock'], win:''},
+    {element:"papper", listen:true, beats : ['rock', 'spock'], win:''},
+    {element:"lizard", listen:true, beats : ['spock', 'papper'], win: ''},
+    {element:"rock", listen:true, beats : ['lizard', 'scissors'], win: ''}]
+    const [result, setResult] = useState('');
     const [options, setOptions] = useState(items);
     const [background, setBackground] = useState(true);
-    const [result, setResult] = useState(false);
 
     function handleClick(e){
-        setOptions([{element:e.currentTarget.id, listen:false}]);
+        let beats = [];
+
+
+        items.forEach(item => {
+            if(item.element === e.currentTarget.id){
+                beats = item.beats;
+            }
+        })
+
+
+        setOptions([{element:e.currentTarget.id, listen:false, beats, win: ''}]);
         setBackground(false);
 
         setTimeout(()=>{
             const number = Math.floor(Math.random()*(4+1));
-            setOptions(current => current.concat({element: items[number].element, listen: false}));
-            setTimeout(()=>{
-                setResult(true);
-            }, 300)
+            setOptions(current => current.concat({element: items[number].element, listen: false, beats: items[number].beats, win:''}));
         },1000)
     }
 
     function restart (){
+        setResult('');
         setOptions(items);
         setBackground(true);
     }
 
     function calc (el1,el2){
-        console.log(el1, el2);
+        let result = 'YOU LOSE';
+
+        if(el1.element === el2.element){
+            result = 'TIE';
+            return result;
+        }
+
+        el1.beats.forEach(el => {
+            if(el === el2.element){
+                result = 'YOU WIN';
+            }
+        })
+
+        return result;
     }
 
     useEffect(()=>{
+
         if(options.length === 2){
-            calc(options[0].element, options[1].element);
+            console.log('hola')
+            setTimeout(()=>{
+                let option1 = options[0],
+                option2 = options[1];
+                let result = calc(options[0], options[1]);
+                if(result === 'YOU WIN'){
+                    option1.win = true;
+                }else if (result === 'YOU LOSE'){
+                    option2.win = true;
+                }else{
+                    option1.win = 'tie';
+                }
+                //setOptions([option1, option2]);
+                setResult(result);
+            }, 500)
         }
     }, [options])
 
 
-    return(
-        <>
+    if(result === ''){
+        return(
+        <div className={styles.container}>
             <section className={background ? styles.options: `${styles.options} ${styles.noBackground}`}>
                 {
-                    options.map((option,index) => <Option key={index} option={option} handleClick={handleClick} />)
+                    options.map((option,index) => <Option key={index} option={option} handleClick={handleClick}/>)
                 }
-
             </section>
-            <div className={result ? styles.result: ''}>
-                You Lose
-                <button onClick={restart}>Play Again</button>
+        </div>
+        )
+    }else{
+        return(
+        <div className={styles.container}>
+                <section className={background ? styles.options: `${styles.options} ${styles.noBackground}`}>
+                {
+                    options.map((option,index) => <Option key={index} option={option} handleClick={handleClick}/>)
+                }
+                </section>
+            <div className={styles["result-group"]}>
+                <span className={styles["result-group__text"]}>{result}</span>
+                <button className={styles["result-group__button"]} onClick={restart}>Play Again</button>
             </div>
-        </>
-    );
+        </div>
+    )};
 }
